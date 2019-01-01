a0033d9a5c0799274ebff6681919baf56a674ee6
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-01-01 19:05:19 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
   def create
     auth_hash = request.env["omniauth.auth"]
+    byebug
     user = User.where(:email =>auth_hash["info"]["email"]).first_or_create do |user|
       user.password = SecureRandom.hex
     end
-    byebug
     session[:user_id] = user.id
     redirect_to root_path
   end
